7dc633b218be89f695e2c4461fec82919bb336c5
bayurisma29 <maulana.bayu@ui.ac.id>
2022-09-14 11:14:14 +0700

    create catalog web

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,6 @@
-# TODO: Implement Routings Here
+from django.urls import path, include
+from .views import show_catalog
+
+urlpatterns = [
+    path('', show_catalog, name='show_catalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+mhs_name = 'Maulana Bayu Risma Santoso Sari'
+studentID = 2106750401
+
+def show_catalog(req):
+    data_catalog_item = CatalogItem.objects.all()
+    context  = {
+        'name' : mhs_name,
+        'studentID': studentID,
+        'list_item': data_catalog_item}
+    return render(req, 'katalog.html', context)
